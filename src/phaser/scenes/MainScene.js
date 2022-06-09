70e5ef242e824a29dab4e5be1f289da58c903663
jaeho seo <jaehoseo@jaehoui-MacBookPro.local>
2022-06-09 22:16:22 +0900

    feature: 게임 입장 시 플레이어 캐릭터 불러오기

--- src/phaser/scenes/MainScene.js
+++ src/phaser/scenes/MainScene.js
@@ -1,23 +1,42 @@
-import { background, characterImage } from "../../constants/assets";
+import Phaser from "phaser";
+import { background } from "../../constants/assets";
 
 export default class MainScene extends Phaser.Scene {
-  constructor(players) {
-    super({
-      key: "GameBackground",
-    });
-
-    this.players = players;
+  constructor() {
+    super({ key: "MainScene" });
+    this.players = null;
   }
 
   preload() {
     this.load.image("background", background.game);
-    this.load.image("character", characterImage[0].path);
+
+    this.load.once("filecomplete", this.loadPlayersOnComplete, this);
   }
 
   create() {
-    this.add.image(960, 540, "background");
-    this.char = this.add.image(500, 600, "character").setScale(3, 3);
+    const keys = this.textures.getTextureKeys();
+
+    for (let i = 0; i < keys.length; i++) {
+      if (keys[i] === "background") {
+        this.add.image(960, 540, "background");
+      } else {
+        const randomDefaultX = Phaser.Math.Between(200, 800);
+        const randomDefaultY = Phaser.Math.Between(500, 800);
+
+        this.physics.add.sprite(randomDefaultX, randomDefaultY, keys[i]).setScale(2, 2);
+      }
+    }
   }
 
-  update() {}
+  loadPlayersOnComplete() {
+    const players = this.players;
+
+    players.forEach(player => {
+      this.load.spritesheet(player.id, player.characterPath, { frameWidth: 32, frameheight: 60 });
+    });
+  }
+
+  set(playersInfo) {
+    this.players = playersInfo;
+  }
 }
