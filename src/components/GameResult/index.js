24947536a2657b7899f315c0acad62b0f2386aba
minjugee <minjugee211@gmail.com>
2022-06-12 05:35:42 +0900

    feature: 남은 시간 계산, 승패 모달 띄우기
    - 총 게임 시간 3분 제한
    - 제한 시간 끝나면 경찰과 도둑 각각 결과 보여주기

---
+++ src/components/GameResult/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { winnerState } from "../../states/modal";
+import { playerState } from "../../states/player";
+import styled from "styled-components";
+
+export default function GameResult() {
+  const winner = useRecoilValue(winnerState);
+  const player = useRecoilValue(playerState);
+
+  return (
+    <div>
+      <Result>{winner === player.role ? <span>{player.role} 승리</span> : <span>{player.role} 패배</span>}</Result>
+    </div>
+  );
+}
+
+const Result = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 30px;
+  font-weight: bold;
+  color: #fff;
+`;
---
+++ src/components/Time/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+import { timeState, winnerState } from "../../states/modal";
+import { socket } from "../../utils/socket";
+import styled from "styled-components";
+
+export default function Time() {
+  const defaultTime = 180;
+
+  const setIsShowingTime = useSetRecoilState(timeState);
+  const setIsResultModalOpen = useSetRecoilState(winnerState);
+  const [remainingTime, setRemainingTime] = useState(defaultTime);
+
+  let second = remainingTime;
+
+  const startGame = second => {
+    const countDown = setInterval(() => {
+      second -= 1;
+
+      if (second === 0) {
+        setIsShowingTime(false);
+        setIsResultModalOpen("robber");
+        clearInterval(countDown);
+      }
+
+      socket.on("send-robber-counts", roleCounts => {
+        if (roleCounts.robber === 0) {
+          setIsShowingTime(false);
+          setIsResultModalOpen("police");
+          clearInterval(countDown);
+        }
+      });
+
+      setRemainingTime(second);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startGame(second);
+  }, []);
+
+  return (
+    <div>
+      <TimeTicker>
+        <span>{"0" + Math.trunc(remainingTime / 60)}</span>
+        <span>:</span>
+        <span>{remainingTime % 60 > 9 ? remainingTime % 60 : "0" + (remainingTime % 60)}</span>
+      </TimeTicker>
+    </div>
+  );
+}
+
+const TimeTicker = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 20px;
+  margin-top: 20px;
+  width: 80px;
+  height: 30px;
+  font-weight: bold;
+  background-color: rgb(255, 255, 255, 0.8);
+  border: 2px solid rgb(0, 0, 0);
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+`;
